834e1dab3505252b03f41f14dc645ec9a47f2d04
Parag Badala <parag.badala@gmail.com>
2018-10-16 19:01:15 +0530

    Add files via upload

---
+++ Project_Assignment/js/index.js
@@ -0,0 +1,14 @@
+// JavaScript Document
+
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("Navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
